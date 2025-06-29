ded01b7481883a82301c814b977dabd6fd0cb4d9
WahyuTomo14 <wahyuxd14@gmail.com>
2025-06-29 22:08:15 +0700

    buat table category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use App\Models\Status;
+use App\Traits\AuditedBySoftDelete;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
-    //
+    use HasFactory, Notifiable, AuditedBySoftDelete, SoftDeletes;
+    protected $table = 'category';
+    protected $guarded = ['id'];
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class, 'status_id');
+    }
 }
--- database/migrations/2025_06_29_150613_create_categories_table.php
+++ database/migrations/2025_06_29_150613_create_categories_table.php
@@ -1,19 +1,23 @@
 <?php
 
+use App\Traits\BaseModelSoftDelete;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    use BaseModelSoftDelete;
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('category', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name', 128);
+            $table->text('desc')->nullable();
+            $this->base($table);
         });
     }
 
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('category');
     }
 };
