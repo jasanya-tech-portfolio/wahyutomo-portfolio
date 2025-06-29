cb587912b4a810e9cbd6f7fff3213f33f310d546
WahyuTomo14 <wahyuxd14@gmail.com>
2025-06-29 22:01:46 +0700

    buat table tools

---
+++ app/Models/Tools.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Status;
+use App\Traits\AuditedBySoftDelete;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Tools extends Model
+{
+    use HasFactory, Notifiable, AuditedBySoftDelete, SoftDeletes;
+    protected $table = 'tools';
+    protected $guarded = ['id'];
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class, 'status_id');
+    }
+}
---
+++ database/migrations/2025_06_29_145252_create_tools_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Traits\BaseModelSoftDelete;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use BaseModelSoftDelete;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tools', function (Blueprint $table) {
+            $table->id();
+            $table->string('logo', 128);
+            $table->string('name', 128);
+            $table->text('desc')->nullable();
+            $this->base($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tools');
+    }
+};
