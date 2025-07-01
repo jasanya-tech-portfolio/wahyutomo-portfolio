1ea0b5842b2edcdb34bc02269ff7c123082a92c9
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-01 20:26:09 +0700

    buat file folder component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,8 @@
+<nav class="bg-white p-4 shadow-sm">
+    <ul class="px-10 items-right justify-end flex space-x-6 text-gray-900 font-medium">
+        <li><a href="#" class="text-xl hover:text-amber-600">Home</a></li>
+        <li><a href="#" class="text-xl hover:text-amber-600">Works</a></li>
+        <li><a href="#" class="text-xl hover:text-amber-600">Blog</a></li>
+        <li><a href="#" class="text-xl hover:text-amber-600">Contact</a></li>
+    </ul>
+</nav>
