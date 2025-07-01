0515bce78a5e2ad56ee86a2c57590613e4056029
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-01 19:56:49 +0700

    buat nav di home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,16 @@
         @vite('resources/css/app.css')
     </head>
     <body>
+        <nav class="bg-white p-4 shadow-sm">
+            <ul class="px-10 items-right justify-end flex space-x-6 text-gray-900 font-medium">
+                <li><a href="#" class="text-xl hover:text-amber-600">Home</a></li>
+                <li><a href="#" class="text-xl hover:text-amber-600">Works</a></li>
+                <li><a href="#" class="text-xl hover:text-amber-600">Blog</a></li>
+                <li><a href="#" class="text-xl hover:text-amber-600">Contact</a></li>
+            </ul>
+        </nav>
         <h1 class="font-bold text-4xl">
-        Hello wahyu!
+            Hello wahyu!
         </h1>
     </body>
 </html>
