ffa8bda957ab8aec59ec354c1731519fff3b11e4
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-02 15:40:00 +0700

    buat blog resource

---
+++ app/Filament/Resources/BlogResource.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use App\Models\Blog;
+use Filament\Tables;
+use App\Models\Status;
+use App\Models\Category;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\BlogResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\BlogResource\RelationManagers;
+
+class BlogResource extends Resource
+{
+    protected static ?string $model = Blog::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-pencil-square';
+
+    protected static ?string $navigationGroup = 'Content Management';
+
+    protected static ?string $navigationLabel = 'Blog';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                FileUpload::make('thumbnail')
+                    ->label('Thumbnail')
+                    ->image()
+                    ->required()
+                    ->directory('blog')
+                    ->columnSpanFull(),
+                TextInput::make('title')
+                    ->required()
+                    ->maxLength(128)
+                    ->reactive()
+                    ->columnSpanFull()
+                    ->afterStateUpdated(function (callable $set, $state) {
+                        $set('slug', str($state)->slug());
+                    }),
+                TextInput::make('slug')
+                    ->required()
+                    ->maxLength(128)
+                    ->reactive()
+                    ->columnSpanFull()
+                    ->afterStateUpdated(function (callable $set, $state) {
+                        $set('slug', str($state)->slug());
+                    }),
+                Select::make('categoris')
+                    ->multiple()
+                    ->relationship('categoris', 'name')
+                    ->label('Category')
+                    ->searchable()
+                    ->options(Category::limit(5)->pluck('name', 'id'))
+                    ->getSearchResultsUsing(function (string $search) {
+                        return Category::where('name', 'like', "%{$search}%")
+                            ->limit(5)
+                            ->pluck('name', 'id')
+                            ->toArray();
+                    })
+                    ->getOptionLabelUsing(function ($value) {
+                        return Category::find($value)?->name;
+                    })
+                    ->required()
+                    ->columnSpanFull(),
+                RichEditor::make('body')
+                    ->required()
+                    ->columnSpanFull(),
+                Select::make('status_id')
+                    ->required()
+                    ->label('Status')
+                    ->searchable()
+                    ->options(Status::all()->pluck('name', 'id')),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('thumbnail')
+                    ->label('Thumbnail')
+                    ->size(70),
+                TextColumn::make('title')
+                    ->searchable()
+                    ->label('Title')
+                    ->searchable(),
+                TextColumn::make('slug')
+                    ->searchable(),
+                TextColumn::make('categoris.name')
+                    ->label('Category')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('body')
+                    ->searchable()
+                    ->html()
+                    ->limit(50),
+                TextColumn::make('status.name')
+                    ->sortable(),
+                TextColumn::make('createdBy.name')
+                    ->label('Created By'),
+                TextColumn::make('updatedBy.name')
+                    ->label("Updated by"),
+                TextColumn::make('deletedBy.name')
+                    ->label("Deleted by"),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBlogs::route('/'),
+            'create' => Pages\CreateBlog::route('/create'),
+            'edit' => Pages\EditBlog::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
