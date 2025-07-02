5a56ca6af0cd28b0a9a9ace3a7000332b3233caf
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-02 15:34:00 +0700

    buat nav label client

--- app/Filament/Resources/ClientResource.php
+++ app/Filament/Resources/ClientResource.php
@@ -24,7 +24,11 @@ class ClientResource extends Resource
 {
     protected static ?string $model = Client::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
+    protected static ?string $navigationGroup = 'Content Management';
+    
+    protected static ?string $navigationLabel = 'Client';
 
     public static function form(Form $form): Form
     {
