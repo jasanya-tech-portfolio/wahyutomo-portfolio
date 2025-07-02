7376662ee6ee8c9bad39e667171fbec1b5f5f75d
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-02 15:11:47 +0700

    tambah get setting di hero title dan text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,11 +20,10 @@
                 <h1 class="text-4xl lg:text-[44px] font-medium py-5 drop-shadow-2xl items-center justify-center font-mono">
                     Hi, I am <span class="font-black text-4xl lg:text-[48px]">{{App\Helpers\SettingHelper::getSetting('name')}}</span>
                     <br>
-                    <span class="text-amber-600 text-3xl lg:text-5xl">Software Engineer</span>
+                    <span class="text-amber-600 text-3xl lg:text-5xl">{{App\Helpers\SettingHelper::getSetting('hero title')}}</span>
                 </h1>
                 <h3 class="py-3 text-lg lg:text-xl text-gray-700 font-light">
-                    I am a Software Engineer. Up to now, I have worked on many projects, 
-                    developing innovative and efficient software solutions.
+                    {{App\Helpers\SettingHelper::getSetting('text hero')}}
                 </h3>
                 <div class="py-5">
                     <a href="#" target="_blank" class="inline-block bg-amber-600 py-2 px-6 rounded shadow hover:shadow-lg hover:bg-amber-700 transition-colors duration-200 max-w-[250px]">
