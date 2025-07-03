25749c09452eeb4e3570942732dfde3aa488c895
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-03 22:20:11 +0700

    card blog bagian date dan category

--- resources/views/components/card/blog.blade.php
+++ resources/views/components/card/blog.blade.php
@@ -3,13 +3,17 @@
         @foreach ($blogs as $blog)
             <div class="bg-white w-[418px] h-[295px] mx-auto shadow-sm hover:shadow-lg">
                 {{-- Title --}}
-                <div class="px-4 py-6">
+                <div class="px-4 pt-6 pb-2">
                     <h1 class="text-[22px] font-semibold hover:underline">
                         {{$blog->title}}
                     </h1>
                 </div>
-                {{-- Date --}}
-                {{-- Category --}}
+                {{-- Date & Category--}}
+                <div class="px-4 py-2">
+                    <h3 class="text-sm font-light">
+                        {{ date('F d, Y', strtotime($blog->created_at)) }} &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; {{$blog->categoris->pluck('name')->join(', ')}} 
+                    </h3>
+                </div>
                 {{-- Body --}}
             </div>
         @endforeach
