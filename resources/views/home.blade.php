b5b8cb1b9368f2f9de29e00ef6973b78bef6df66
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-03 20:01:56 +0700

    fix hero section di home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     {{-- hero section --}}
     {{-- <div class="bg-white py-10 px-10 rounded-b-4xl"> --}}
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 py-3 lg:py-5 item-center justify-center">
-            <div class="bg-white py-3 order-1 md:order-2 lg:order-2">
+            <div class="py-3 order-1 md:order-2 lg:order-2">
                 <div class="flex items-center justify-center h-full">
                     <img src="{{ asset('images/profile.jpg') }}"
                         alt="Profile Image"
@@ -16,7 +16,7 @@
                         transition-shadow duration-300 ease-in-out">
                 </div>
             </div>
-            <div class="bg-white text-gray-900 flex flex-col justify-center px-2 lg:px-3 order-2 md:order-2 lg:order-1 text-center lg:text-left">
+            <div class="text-gray-900 flex flex-col justify-center px-2 lg:px-3 order-2 md:order-2 lg:order-1 text-center lg:text-left">
                 <h1 class="text-4xl lg:text-[44px] font-medium py-5 drop-shadow-2xl items-center justify-center font-mono">
                     Hi, I am <span class="font-black text-4xl lg:text-[48px]">{{App\Helpers\SettingHelper::getSetting('name')}}</span>
                     <br>
