09038de4cf10d5fd0fce4fec7efcff1f3ce8d0d8
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-03 22:43:40 +0700

    fix card blog title

--- resources/views/components/card/blog.blade.php
+++ resources/views/components/card/blog.blade.php
@@ -3,23 +3,23 @@
         Recent Blogs
     </h1>
     <div class="h-[5px] w-[150px] bg-amber-600 mx-auto"></div>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-2 py-12 font-[poppins]">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-2 py-12 font-[poppins] text-gray-800">
         @foreach ($blogs as $blog)
-            <div class="bg-white w-[418px] h-[295px] mx-auto my-auto shadow-sm hover:shadow-lg border-gray-300 hover:border-2">
+            <div class="bg-white w-[418px] h-[280px] mx-auto my-auto shadow-sm hover:shadow-lg border-gray-300 hover:border-2">
                 {{-- Title --}}
-                <div class="px-4 pt-6 pb-2">
+                <div class="px-4 pt-6 pb-2 w-[374px] h-[85px]">
                     <h1 class="text-[22px] font-semibold hover:underline">
-                        {{$blog->title}}
+                        {{ \Illuminate\Support\Str::limit($blog->title, 50, '...') }}
                     </h1>
                 </div>
                 {{-- Date & Category--}}
-                <div class="px-4 py-2">
+                <div class="px-4 py-4">
                     <h3 class="text-sm font-light">
                         {{ date('F d, Y', strtotime($blog->created_at)) }} &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; {{$blog->categoris->pluck('name')->join(', ')}} 
                     </h3>
                 </div>
                 {{-- Body --}}
-                <div class="px-4 py-4">
+                <div class="px-4 py-2">
                     <p class="text-sm line-clamp-3">{{ strip_tags($blog->body) }}</p>
                 </div>
             </div>
