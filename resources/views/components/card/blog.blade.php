5e312ae08d0fafd361de4600d10e0ab91c8356c3
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-03 22:28:22 +0700

    card blog bagian body dan tulisan atas

--- resources/views/components/card/blog.blade.php
+++ resources/views/components/card/blog.blade.php
@@ -1,7 +1,11 @@
 <div class="bg-gray-200">
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 py-12 font-[poppins]">
+    <h1 class="text-2xl md:text-3xl text-gray-800 font-[poppins] font-medium text-center pt-10 pb-4">
+        Recent Blogs
+    </h1>
+    <div class="h-[5px] w-[150px] bg-amber-600 mx-auto"></div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-12 font-[poppins]">
         @foreach ($blogs as $blog)
-            <div class="bg-white w-[418px] h-[295px] mx-auto shadow-sm hover:shadow-lg">
+            <div class="bg-white w-[418px] h-[295px] mx-auto my-auto shadow-sm hover:shadow-lg">
                 {{-- Title --}}
                 <div class="px-4 pt-6 pb-2">
                     <h1 class="text-[22px] font-semibold hover:underline">
@@ -15,6 +19,9 @@
                     </h3>
                 </div>
                 {{-- Body --}}
+                <div class="px-4 py-4">
+                    <p class="text-sm line-clamp-3">{{ strip_tags($blog->body) }}</p>
+                </div>
             </div>
         @endforeach
     </div>
