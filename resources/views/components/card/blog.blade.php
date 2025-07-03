412c5af83e63cef8e68318f8d75796882e00f945
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-03 22:35:45 +0700

    buat border card blog

--- resources/views/components/card/blog.blade.php
+++ resources/views/components/card/blog.blade.php
@@ -1,11 +1,11 @@
-<div class="bg-gray-200">
+<div class="bg-gray-100">
     <h1 class="text-2xl md:text-3xl text-gray-800 font-[poppins] font-medium text-center pt-10 pb-4">
         Recent Blogs
     </h1>
     <div class="h-[5px] w-[150px] bg-amber-600 mx-auto"></div>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-12 font-[poppins]">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-2 py-12 font-[poppins]">
         @foreach ($blogs as $blog)
-            <div class="bg-white w-[418px] h-[295px] mx-auto my-auto shadow-sm hover:shadow-lg">
+            <div class="bg-white w-[418px] h-[295px] mx-auto my-auto shadow-sm hover:shadow-lg border-gray-300 hover:border-2">
                 {{-- Title --}}
                 <div class="px-4 pt-6 pb-2">
                     <h1 class="text-[22px] font-semibold hover:underline">
