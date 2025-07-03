b595e6c60183f3f17e06ab44ec06f93cb5f58008
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-03 20:11:16 +0700

    buat method index dan get data tools di home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tools;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    //method get data untuk halaman home
+    public function index()
+    {
+        //ambil data tools
+        $tools = Tools::where('status_id', 1)
+            ->get();
+        
+        return view("pages.home", compact('tools'));
+    }
 }
