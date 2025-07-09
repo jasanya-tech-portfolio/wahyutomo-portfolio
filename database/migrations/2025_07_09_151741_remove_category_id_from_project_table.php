f1ce09a9f71e071595ece5eb31251db051dbbe9c
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-09 22:19:06 +0700

    buat table untuk hapus kolom di tabel project

---
+++ database/migrations/2025_07_09_151741_remove_category_id_from_project_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('project', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+            $table->dropColumn('category_id');
+            $table->dropForeign(['tools_id']);
+            $table->dropColumn('tools_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('project', function (Blueprint $table) {
+            //
+        });
+    }
+};
