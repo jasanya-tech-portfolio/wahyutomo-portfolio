23b67519a484540e34ceb15e7b6dc7b47aa46263
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-03 23:37:32 +0700

    update base blade

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,32 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>@yield('title', 'My App')</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
-    
-    {{-- Template CSS --}}
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <head>
+        <meta charset="UTF-8">
+        <title>@yield('title', 'My App')</title>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description"
+            content="An impressive and flawless site template that includes various UI elements and countless features, attractive ready-made blocks and rich pages, basically everything you need to create a unique and professional website.">
+        <meta name="keywords"
+            content="Tailwind CSS, business, corporate, creative, gulp, marketing, minimal, modern, multipurpose, one page, responsive, saas, sass, seo, startup, html5 template, site template">
+        <meta name="author" content="elemis">
+        @vite('resources/css/app.css')
+        {{-- <title>{{ $title ?? 'JASANYATECH' }}</title> --}}
+        {{-- <link rel="icon" href="images/2.png" type="image/png" sizes="16x16"> --}}
+        <link rel="shortcut icon" href="{{ asset('assets/img/icon_web.ico') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/fonts/unicons/unicons.css">
+        <link rel="stylesheet" href="{{ asset('assets') }}/css/plugins.css">
+        <link rel="stylesheet" href="{{ asset('assets') }}/style.css">
+        <link rel="stylesheet" href="{{ asset('assets') }}/css/colors/grape.css">
+        <link rel="preload" href="{{ asset('assets') }}/css/fonts/space.css" as="style" onload="this.rel='stylesheet'">
+        <style>
+            .swiper-container.nav-color .swiper-button,
+            .swiper-container.nav-color .swiper-slide figure .item-link {
+                background: #605dbae6 !important;
+                color: #fff !important;
+            }
 
-    {{-- Template JS --}}
-    <script src="{{ asset('assets/js/main.js') }}" defer></script>
+            .swiper-container.nav-color .swiper-button:hover,
+            .swiper-container.nav-color .swiper-slide figure .item-link:hover {
+                background: #605dba !important;
+            }
 
-    @vite('resources/css/app.css')
-</head>
-<body class="bg-white text-gray-900">
+            @media (max-width: 991.98px) {
+                .navbar-expand-lg .navbar-collapse .dropdown-toggle:after {
+                    color: #ffffff !important;
+                }
+            }
+        </style>
+    </head>
+    <body class="bg-white text-gray-900">
 
-    {{-- Include Navbar --}}
-    @include('components.navbar')
+        {{-- Include Navbar --}}
+        @include('components.navbar')
 
-    {{-- Page Content --}}
-    <main class="min-h-screen">
-        @yield('content')
-    </main>
+        {{-- Page Content --}}
+        <main class="min-h-screen">
+            @yield('content')
+        </main>
 
-    {{-- Include Footer --}}
-    @include('components.footer')
+        {{-- Include Footer --}}
+        @include('components.footer')
 
-    @vite('resources/js/app.js')
-</body>
+        <script src="{{ asset('assets') }}/js/plugins.js"></script>
+        <script src="{{ asset('assets') }}/js/theme.js"></script>
+    </body>
 </html>
