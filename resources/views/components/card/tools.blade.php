0377199f3828701b1f069dc00fe9e77fcd5255e8
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-04 15:37:47 +0700

    fix tools card

--- resources/views/components/card/tools.blade.php
+++ resources/views/components/card/tools.blade.php
@@ -1,4 +1,4 @@
-<section class="wrapper !bg-[#ffffff]  wrapper-border mx-auto">
+<section class="flex justify-center wrapper !bg-[#ffffff] wrapper-border mx-auto">
                   <div class="container pt-20 xl:pt-28 lg:pt-28 md:pt-28 pb-16 xl:pb-20 lg:pb-20 md:pb-20">
                     <p class="text-center !mb-8">Trusted by over 2K+ clients across the world</p>
                     <div class="swiper-container clients !mb-0 relative z-10" data-margin="30" data-dots="false" data-loop="true" data-autoplay="true" data-autoplaytime="1" data-drag="false" data-speed="5000" data-items-xxl="7" data-items-xl="6" data-items-lg="5" data-items-md="4" data-items-xs="2">
