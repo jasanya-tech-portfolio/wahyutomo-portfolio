95235fbf193df3093128bbe95addec746dbe6ea1
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-06 12:52:27 +0700

    edit py dan angka di component count

--- resources/views/components/card/count.blade.php
+++ resources/views/components/card/count.blade.php
@@ -1,5 +1,5 @@
 <section class="flex justify-center wrapper !bg-[#ffffff] ">
-    <div class="container pt-20 pb-16">
+    <div class="container py-8">
         <div class="flex flex-wrap mx-[-15px]">
             <div class="xl:w-10/12 w-full flex-[0_0_auto] !px-[15px] max-w-full !mx-auto">
                 <div class="card image-wrapper bg-full bg-image bg-overlay bg-cover [background-size:100%] bg-[center_center] bg-no-repeat !bg-scroll !relative z-0 before:rounded-[0.4rem] before:bg-[rgba(30,34,40,.4)] before:content-[''] before:block before:absolute before:z-[1] before:w-full before:h-full before:left-0 before:top-0" data-image-src="../../assets/img/photos/bg2.jpg">
@@ -26,7 +26,7 @@
 
                             <!--column -->
                             <div class="w-6/12 xl:w-3/12 lg:w-3/12 flex-[0_0_auto] !px-[15px] max-w-full !mt-[20px] text-white">
-                                <h3 class="counter counter-lg text-white xl:!text-[2.2rem] !text-[calc(1.345rem_+_1.14vw)] !tracking-[normal] !leading-none !mb-2">23K</h3>
+                                <h3 class="counter counter-lg text-white xl:!text-[2.2rem] !text-[calc(1.345rem_+_1.14vw)] !tracking-[normal] !leading-none !mb-2">23957</h3>
                                 <p class="!m-0">Code Lines</p>
                             </div>
 
