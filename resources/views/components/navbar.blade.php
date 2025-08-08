b21b39b136fc379ed3442088e1126ba7b2369925
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-09 00:03:58 +0700

    tambah logo di navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav id="navbar" class="bg-white/75 p-2 fixed top-0 left-0 right-0 z-50">
+<nav id="navbar scroll" class="bg-white/75 p-2 fixed top-0 left-0 right-0 z-50">
     {{-- Logo --}}
     <div class="flex justify-end items-right px-4">
         {{-- Hamburger Button --}}
@@ -20,13 +20,20 @@
     </div>
 
     {{-- Navigation Links --}}
-    <ul
-        id="menu"
-        class="px-10 space-x-8 text-gray-900 font-medium hidden lg:flex lg:justify-end lg:items-center"
-    >
-        <li><a href="/" class="text-lg hover:text-amber-600 block py-2">Home</a></li>
-        <li><a href="#project" class="text-lg hover:text-amber-600 block py-2">Project</a></li>
-        <li><a href="#" class="text-lg hover:text-amber-600 block py-2">Blog</a></li>
-        <li><a href="#contact" class="text-lg hover:text-amber-600 block py-2">Contact</a></li>
-    </ul>
+    <div class="flex items-center justify-between px-10">
+        {{-- Logo --}}
+        <a href="/" class="flex items-center">
+            <img src="{{App\Helpers\BannerHelper::getBannerImageUrl('logo')}}" alt="Logo" class="h-10 w-auto">
+        </a>
+        {{-- Navigation Links --}}
+        <ul
+            id="menu"
+            class="px-10 space-x-8 text-gray-900 font-medium hidden lg:flex lg:justify-end lg:items-center"
+        >
+            <li><a href="/" class="text-lg hover:text-amber-600 block py-2">Home</a></li>
+            <li><a href="#project" class="text-lg hover:text-amber-600 block py-2">Project</a></li>
+            <li><a href="#" class="text-lg hover:text-amber-600 block py-2">Blog</a></li>
+            <li><a href="#contact" class="text-lg hover:text-amber-600 block py-2">Contact</a></li>
+        </ul>
+    </div>
 </nav>
