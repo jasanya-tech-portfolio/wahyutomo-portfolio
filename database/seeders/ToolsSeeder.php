8b8da9bccb81bcfbe11b8a0e961021499891c122
WahyuTomo14 <wahyuxd14@gmail.com>
2025-08-15 23:07:13 +0700

    tambah kolom di seeder tools

--- database/seeders/ToolsSeeder.php
+++ database/seeders/ToolsSeeder.php
@@ -18,36 +18,42 @@ class ToolsSeeder extends Seeder
                 'logo' => 'default.png',
                 'name' => 'PHP',
                 'desc' => '',
+                'percentage' => '80',
                 'status_id' => 1
             ],
             [
                 'logo' => 'default.png',
                 'name' => 'Laravel',
                 'desc' => '',
+                'percentage' => '90',
                 'status_id' => 1
             ],
             [
                 'logo' => 'default.png',
                 'name' => 'Filament',
                 'desc' => '',
+                'percentage' => '95',
                 'status_id' => 1
             ],
             [
                 'logo' => 'default.png',
                 'name' => 'Tailwind',
                 'desc' => '',
+                'percentage' => '85',
                 'status_id' => 1
             ],
             [
                 'logo' => 'default.png',
                 'name' => 'Python',
                 'desc' => '',
+                'percentage' => '85',
                 'status_id' => 1
             ],
             [
                 'logo' => 'default.png',
                 'name' => 'REST API',
                 'desc' => '',
+                'percentage' => '80',
                 'status_id' => 1
             ],
         ];
